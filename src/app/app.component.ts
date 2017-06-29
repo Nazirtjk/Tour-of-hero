3d9969865c73366529e88c9b9bd68fde25e047f2
Admin <superboy4@mail.ru>
2017-06-29 22:00:51 +0500

    sozdal 2 button pri najatii kotorih po ocheredi pokazivoyt page1 1 button a 2 button pri najatii pokazivaet page2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+     styleUrls: ['./app.component.css'],
+    // template: `
+    //     <button (click)="toggle()">toggle</button>
+    //     <div [ngClass]="{invisible: visibility}">
+    //     <h1> Hello Angular 2</h1>
+    //     <p>
+    //         Angular 2 представляет модульную архитектуру приложения
+    //     </p>
+    // </div>
+    // `,
+     styles: [`.invisible{display:none}`]
 })
 export class AppComponent {
   title = 'app works!';
   name = '';
-}
 
+}
--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -6,8 +6,15 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-root',
     templateUrl: './home.html',
-
+    styles: [`.invisible{display:none}`]
 })
 export class HomeComponent {
-
+    visibility: boolean = true;
+    toggle(){
+        this.visibility=!this.visibility;
+    }
+    condition: boolean = true;
+    toggle1(){
+        this.condition=!this.condition;
+    }
 }
